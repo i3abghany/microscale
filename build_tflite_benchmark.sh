71afe87dbb31bdf74c78bcc06159386729ae3816
Mahmoud Abumandour <mam47@sfu.ca>
2023-08-01 22:19:50 -0700

    Add anomaly detection

--- build_tflite_benchmark.sh
+++ build_tflite_benchmark.sh
@@ -2,7 +2,15 @@
 
 TARGET_ARCH=riscv64
 TOOLCHAIN_PREFIX=riscv64-unknown-linux-gnu-
+BENCHMARK_NAME=$1
+if test -z $BENCHMARK_NAME
+then
+    echo "Usage: $0 BENCHMARK_NAME"
+    echo "Benchmarks supported are: keyword_spotting"
+    echo "                          anomaly_detection"
+    exit -1
+fi
 
 cd tflite-micro
-make -f tensorflow/lite/micro/tools/make/Makefile test_keyword_spotting_test TOOLCHAIN_PREFIX=$TOOLCHAIN_PREFIX TARGET_ARCH=$TARGET_ARCH -j48
-cp gen/linux_riscv64_default/bin/keyword_spotting_test ../kws_test_riscv
+make -f tensorflow/lite/micro/tools/make/Makefile test_$BENCHMARK_NAME\_test TOOLCHAIN_PREFIX=$TOOLCHAIN_PREFIX TARGET_ARCH=$TARGET_ARCH -j48
+cp gen/linux_riscv64_default/bin/$BENCHMARK_NAME\_test ../$BENCHMARK_NAME\_test
--- run_system.sh
+++ run_system.sh
@@ -1,14 +1,22 @@
 M5_PATH=$1
+BENCHMARK_EXECUTABLE=$2
 
 if [[ "$M5_PATH" = "" ]]
 then
-    echo "usage: $0 M5_PATH"
+    echo "usage: $0 M5_PATH BENCHMARK_EXECUTABLE"
     exit -1
 fi
 
-$M5_PATH/build/RISCV/gem5.opt                    \
+if [[ "$BENCHMARK_EXECUTABLE" = "" ]]
+then
+    echo "usage: $0 M5_PATH BENCHMARK_EXECUTABLE"
+    exit -1
+fi
+
+$M5_PATH/build/RISCV/gem5.fast                   \
+    --outdir=out/m5out_$BENCHMARK_EXECUTABLE     \
     $M5_PATH/configs/example/se.py               \
-    --cmd=./kws_test_riscv                       \
+    --cmd=./$BENCHMARK_EXECUTABLE                \
     --sys-clock=225MHz                           \
     --cpu-clock=225MHz                           \
     --cpu-type=MinorCPU                          \
@@ -18,10 +26,10 @@ $M5_PATH/build/RISCV/gem5.opt                    \
     --caches                                     \
     --l1d_size=32kB                              \
     --l1i_size=32kB                              \
-    --l1d_assoc=8                                \
-    --l1i_assoc=8                                \
+    --l1d_assoc=4                                \
+    --l1i_assoc=4                                \
     --l1d-hwp-type=StridePrefetcher              \
     --cacheline_size=64                          \
     --mem-type=LPDDR3_1600_1x32                  \
     --mem-size=256MB                             \
-    --bp-type=BiModeBP                           \
+    --bp-type=BiModeBP                           &
