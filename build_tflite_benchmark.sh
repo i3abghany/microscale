95fe65f969231bc1af2c5a52aaca4ffd77a8393a
Mahmoud Abumandour <mam47@sfu.ca>
2023-08-01 22:19:50 -0700

    Add visual wake words

--- build_tflite_benchmark.sh
+++ build_tflite_benchmark.sh
@@ -8,9 +8,10 @@ then
     echo "Usage: $0 BENCHMARK_NAME"
     echo "Benchmarks supported are: keyword_spotting"
     echo "                          anomaly_detection"
+    echo "                          visual_wake_words"
     exit -1
 fi
 
 cd tflite-micro
 make -f tensorflow/lite/micro/tools/make/Makefile test_$BENCHMARK_NAME\_test TOOLCHAIN_PREFIX=$TOOLCHAIN_PREFIX TARGET_ARCH=$TARGET_ARCH -j48
-cp gen/linux_riscv64_default/bin/$BENCHMARK_NAME\_test ../$BENCHMARK_NAME\_test
+cp gen/linux_riscv64_default/bin/$BENCHMARK_NAME\_test ../$BENCHMARK_NAME\_test_123123
